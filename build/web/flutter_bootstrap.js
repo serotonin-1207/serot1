b42a0035649a8bdf3b11e8e4019131898fe42852
serotonin-1207 <serotonin.1207@gmail.com>
2026-06-08 16:16:18 +0900

    진짜 최종 UI 파일 빌드 후 반영

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2456267824" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "647452067" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
